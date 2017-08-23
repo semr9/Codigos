3f19147c107c0daca093b3c0d34acdcb7dfd38de
semr9 <sebastian.moscoso@ucsp.edu.pe>
2017-08-22 21:11:53 -0500

    poniendo los codigos

---
+++ resultado_holamundo/CMakeFiles/hola.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hola.dir/holamundo.cpp.o"
+  "hola.pdb"
+  "hola"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hola.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
